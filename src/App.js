7ff8d1ede324792441981cc5d4ae9277ef613cc3
yashasvi13 <yashasvij13@gmail.com>
2019-04-18 17:51:04 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
 import React, { Component } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Navbar from "./components/layout/Navbar";
-import ChatApp from "./components/Chat/ChatApp";
-import ChatList from "./components/Chat/ChatList";
-
-import SignIn from "./components/auth/SignIn";
-import SignUp from "./components/auth/SignUp";
-// import NewMessageForm from "./components/form/NewMessageForm";
 
 class App extends Component {
   render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar />
-
-          <ChatList />
-          {/* <NewMessageForm /> */}
-          <Switch>
-            <Route path="/" component={ChatApp} />
-            <Route path="/signin" component={SignIn} />
-            <Route path="/signup" component={SignUp} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    );
+    return <div className="App" />;
   }
 }
 
