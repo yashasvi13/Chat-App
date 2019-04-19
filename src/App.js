cfc9cb2b65348a4364103c7b3ba2880b4247edde
yashasvi13 <yashasvij13@gmail.com>
2019-04-19 20:44:18 +0530

    redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import AppWindow from "./components/ChatApp/AppWindow";
-
+import firebase from "./firebase";
 class App extends Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      console.log(user);
+      if (user) {
+        this.props.history.push("/");
+      }
+    });
+  }
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Switch>
-            <Route exact path="/" component={AppWindow} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-          </Switch>
-        </div>
-      </Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={AppWindow} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </div>
     );
   }
 }
